edf30aa0853b7deee2b4ff6976326bf47dec4fb2
VisionaryWebDesigner <visionarywebdesigner@gmail.com>
2026-02-05 05:56:42 +0000

    Can we use this for Rose day icons <img width="48" height="48" src="htt

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
